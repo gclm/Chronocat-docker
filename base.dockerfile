054abe7bd2f8a90789a9260c19a3ffea9c618ed5
Il Harper <hi@ilharper.com>
2023-10-04 13:55:09 +0800

    feat: add base image

---
+++ base.dockerfile
@@ -0,0 +1,46 @@
+# syntax=docker/dockerfile:1
+
+FROM phusion/baseimage:jammy-1.0.1
+
+ARG BUILD_ARCH
+
+ENV DEBIAN_FRONTEND=noninteractive
+
+COPY --chmod=0755 rootfs /
+
+RUN apt update && \
+  \
+  # 安装依赖
+  apt install -y \
+  unzip \
+  xorg xvfb openbox python3-xdg python3-numpy && \
+  \
+  # 下载 QQNT
+  curl -fsSLo /tmp/qqnt.deb https://dldir1.qq.com/qqfile/qq/QQNT/ad5b5393/linuxqq_3.1.2-13107_${BUILD_ARCH}.deb && \
+  apt install -y /tmp/qqnt.deb && \
+  \
+  # 下载 LiteLoaderQQNT
+  cd /opt/QQ/resources/app && \
+  curl -fsSLo LiteLoaderQQNT.zip https://github.com/LiteLoaderQQNT/LiteLoaderQQNT/releases/download/0.5.3/LiteLoaderQQNT.zip && \
+  \
+  # 安装 LiteLoaderQQNT
+  unzip LiteLoaderQQNT.zip && \
+  rm LiteLoaderQQNT.zip && \
+  sed -i 's/.\/app_launcher\/index.js/.\/LiteLoader/' package.json && \
+  cd / && \
+  # 清理
+  apt autoremove -y && \
+  apt clean && \
+  rm -rf \
+  /var/lib/apt/lists/* \
+  /tmp/* \
+  /var/tmp/*
+
+# Chronocat Red
+EXPOSE 16530/tcp
+
+# Chronocat Satori
+EXPOSE 5500/tcp
+
+# Chronocat Admin
+EXPOSE 16340/tcp
---
+++ build-base.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+BUILD_MODE=${1:-build}
+
+BUILD_AUTO_TAG=$(git rev-parse --abbrev-ref HEAD)-$(git rev-list --count HEAD)
+BUILD_TAG=${BUILD_TAG:-${BUILD_AUTO_TAG}}
+BUILD_DOCKER_BUILDER=${BUILD_DOCKER_BUILDER:-container}
+
+BUILD_ARCH_LIST=(
+  amd64
+  arm64
+)
+
+echo "Building chronoc/base:${BUILD_TAG} using builder: ${BUILD_DOCKER_BUILDER}\n\n"
+
+for BUILD_ARCH in ${BUILD_ARCH_LIST[@]};
+do
+  BUILD_PLATFORM=linux/${BUILD_ARCH}
+
+  BUILD_IMAGE_ARCH_TAG=${BUILD_TAG}-linux-${BUILD_ARCH}
+
+  case ${BUILD_MODE} in
+  "push")
+      docker buildx build \
+        --push \
+        --builder=${BUILD_DOCKER_BUILDER} \
+        --build-arg BUILD_ARCH=${BUILD_ARCH} \
+        --platform ${BUILD_PLATFORM} \
+        -f base.dockerfile \
+        -t ghcr.io/chrononeko/base:${BUILD_IMAGE_ARCH_TAG} \
+        .
+      ;;
+    *)
+      docker buildx build \
+        --builder=${BUILD_DOCKER_BUILDER} \
+        --build-arg BUILD_ARCH=${BUILD_ARCH} \
+        --platform ${BUILD_PLATFORM} \
+        -f base.dockerfile \
+        -t ghcr.io/chrononeko/base:${BUILD_IMAGE_ARCH_TAG} \
+        .
+      ;;
+  esac
+done
